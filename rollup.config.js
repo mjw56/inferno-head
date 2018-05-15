fa1305a59cc9ae31349d795adc23695e4c427090
Bogdan Chadkin <trysound@yandex.ru>
2018-05-16 00:10:02 +0300

    Bundle cjs and esm with rollup
    In this diff I added rollup to bundle cjs and esm. With flat bundles we
    can easier to control the size user consumes.
    
    size snapshot plugin allows to track that size with every commit.
    Moreover it shows treeshakability of the lib for esm bundle.
    
    It can be less critical for user bundle but make sense when the lib is
    used by another one with a set of components. So one unused component
    depending on this lib will not keep this dependency in user bundle.

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import babel from 'rollup-plugin-babel';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import pkg from './package.json';
+
+const input = './src/index.js';
+
+const external = id => !id.startsWith('\0') && !id.startsWith('/') && !id.startsWith('.');
+
+export default [
+  {
+    input,
+    output: { file: pkg.main, format: 'cjs', exports: 'named' },
+    external,
+    plugins: [babel({ plugins: ['external-helpers'] }), sizeSnapshot()],
+  },
+
+  {
+    input,
+    output: { file: pkg.module, format: 'es', exports: 'named' },
+    external,
+    plugins: [babel({ plugins: ['external-helpers'] }), sizeSnapshot()],
+  },
+];
